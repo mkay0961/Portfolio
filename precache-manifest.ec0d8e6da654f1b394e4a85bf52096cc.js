d58ebd7ade5ae42be26d06e4114c49abee08c9e2
mkay0961 <mkay0961@gmail.com>
2019-04-01 22:13:46 -0400

    Updates

---
+++ precache-manifest.ec0d8e6da654f1b394e4a85bf52096cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "43c0553b557ecf1ced1b",
+    "url": "/static/css/main.dae9544d.chunk.css"
+  },
+  {
+    "revision": "43c0553b557ecf1ced1b",
+    "url": "/static/js/main.e59f2849.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "da2da1932c3e290f1005fa280df40b41",
+    "url": "/index.html"
+  }
+];
