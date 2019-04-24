c90f9fa528d0fcaadad18107eeac56abc625343a
mkay0961 <mkay0961@gmail.com>
2019-04-24 15:41:06 -0400

    added brower router

---
+++ src/components/navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const navbar = (props) => (
+  <div>
+      navbar
+  </div>
+)
+
+export default navbar
---
+++ src/containers/frontPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const frontPage = (props) => (
+  <div>
+      frontPage
+  </div>
+)
+
+export default frontPage
